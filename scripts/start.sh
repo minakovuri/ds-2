e8c001bbaad9cb0f294cab866dac7c9582c93b52
minakovuri <minakov_uri@mail.ru>
2020-03-25 23:44:32 +0300

    Добавил опциональную уствновку необходимой утилиты парсинга JSON

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
 # To parse JSON params, you should run `sudo apt install jq`
+JQ_OK=$(dpkg-query -W --showformat='${Status}\n' jq | grep "install ok installed")
+if [ "" == "$JQ_OK" ]; then
+    echo " --------- INSTALLING JSON PARSER ---------- "
+    sudo apt install jq
+fi
+
 BACKEND_API_HOST=$( jq .BackendApi.host ../config/config.json | tr -d '"' )
 BACKEND_API_PORT=$( jq .BackendApi.port ../config/config.json )
 FRONTEND_PORT=$( jq .Frontend.port ../config/config.json )
+
+echo " --------- START CONTAINERS ---------- "
 
 # Run BackendApi
 docker run -e ASPNETCORE_URLS=http://+:${BACKEND_API_PORT} --rm -d -p ${BACKEND_API_PORT}:${BACKEND_API_PORT} --name backend-api ds-2/backend-api
